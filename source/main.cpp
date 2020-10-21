f2177959047e2db861a73440f00eb20db0b23ace
DoodlingTurtle <52770682+DoodlingTurtle@users.noreply.github.com>
2020-10-21 19:12:59 +0200

    added a Background using the new RGNDS::GL2D::glPixel function

--- source/main.cpp
+++ source/main.cpp
@@ -5,6 +5,7 @@
 -----------------------------------------------------------------------------*/
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <climits>
 
 #include <nds.h>
@@ -21,8 +22,17 @@
 class PixelEngine : public RGNDS::Engine, public Broadcast::Listener {
 
 private:
+
+    struct Star {
+        int x;
+        int y;
+        int alpha;
+    };
+
+
     Ship ship;
     Asteroid asteroids[MAX_ASTEROIDS];
+    std::vector<Star> stars;
 
     RGNDS::Transform scorelocation;
     float scoreTimer;
@@ -31,7 +41,7 @@ private:
         if(channel == bchAsteroid) {
             switch(event) {
             case bceHitPlayer:
-                exit();     // Meteor hits player = game over;
+                //exit();     // Meteor hits player = game over;
                 break;
             case bceDead:
                 ((Asteroid*)sender)->kill();
@@ -57,6 +67,15 @@ protected:
 
         score = 0;
         scoreTimer = 0.0f;
+        game_difficulty = 4;
+
+        for(a = 0; a < 64; a++) {
+            stars.push_back((const Star){
+                    rand() / (INT_MAX/256),
+                    rand() / (INT_MAX/(SCREEN_HEIGHT*2)),
+                    rand() / (INT_MAX/15) + 16
+            });
+        }
 
         for(a = 0; a < (int)game_difficulty; a++) {
             asteroids[a].bringBackToLife(ship.pos, false, 1);
@@ -74,6 +93,7 @@ protected:
         }
 
         Asteroid::broadcast.unsubscribe(this);
+        stars.clear();
     }
 
     void onUpdate(float deltaTime) {
@@ -101,6 +121,10 @@ protected:
 
     void onDraw(float deltaTime, int screen) {
         int a;
+
+        for(auto star : stars) {
+            RGNDS::GL2D::glPixel(star.x, star.y, Engine_Color16(1, 15, 15, 15), star.alpha);
+        }
 
         for(a = 0; a < MAX_ASTEROIDS; a++)
             asteroids[a].draw(screen);
