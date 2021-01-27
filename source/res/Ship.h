9cfab1d6dd1150b9268ff1f861305459d1e80c60
DoodlingTurtle <52770682+DoodlingTurtle@users.noreply.github.com>
2021-01-27 16:29:42 +0100

    Added sprite graphics for ship and shield

---
+++ source/res/Ship.h
@@ -0,0 +1,10 @@
+#ifndef __SHIP_H__
+#define __SHIP_H__ 1
+
+#define _SHIP_PAL_FILE "nitro:/pictures/Ship.pal.bin"
+#define _SHIP_PAL_SIZE 8
+
+#define _SHIP_FILE "nitro:/pictures/Ship.bin"
+#define _SHIP_SIZE 3072
+
+#endif
